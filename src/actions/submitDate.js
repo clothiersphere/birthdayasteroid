36dbb0f8a7b23978d7001f0876f8d2199c08351f
Christopher Lee <xchrislee@gmail.com>
2018-06-04 12:35:27 -0500

    Create action to pass date to API and pass returned asteroid information to asteroidList reducer.

--- src/actions/submitDate.js
+++ src/actions/submitDate.js
@@ -1,8 +1,22 @@
 import * as actionTypes from '../constants/actionTypes';
+import axios from 'axios';
 
-export function submitDate(date) {
+function getAsteroid(date) {
   return {
     type: actionTypes.SUBMIT_DATE,
     date,
+  };
+}
+
+export function submitDate(date) {
+  return function (dispatch) {
+    const request = axios({
+      method: 'GET',
+      url: `http://localhost:8080/api/birthday/${date}`,
+    });
+    return request
+      .then((response) => {
+        dispatch(getAsteroid(response.data));
+      });
   };
 }
