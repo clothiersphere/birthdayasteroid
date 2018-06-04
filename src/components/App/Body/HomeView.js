673f29e5c35f92111ba5d6a0e79f4a59c68dd458
Christopher Lee <xchrislee@gmail.com>
2018-06-04 15:00:34 -0500

    Modify component to display NEO count & PHA count

--- src/components/App/Body/HomeView.js
+++ src/components/App/Body/HomeView.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const HomeView = () => (
-  <div className="home-view">
-        Asteroids
-  </div>
-);
+const HomeView = (props) => {
+  let neoLength = 0;
+  let phaLength = 0;
+
+  if (props.neo.nearEarthObjects) {
+    const { nearEarthObjects } = props.neo;
+    if (nearEarthObjects.length) {
+      neoLength = nearEarthObjects.length;
+    }
+
+    const pha = nearEarthObjects.filter(neo => neo.is_potentially_hazardous_asteroid === 'true');
+    phaLength = pha.length;
+  }
+
+  return (
+    <div className="home-view">
+      <div>
+        There are {neoLength} near earth objects.
+      </div>
+      <div>
+        {phaLength} of those are potentially hazardous asteroids.
+      </div>
+    </div>
+  );
+};
 
 export default HomeView;
