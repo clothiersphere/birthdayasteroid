a5ac49bfd0a8a24c72bb32a2177287e05a0f66cf
Christopher Lee <xchrislee@gmail.com>
2018-06-04 14:33:36 -0500

    Pull HomeView & BirthdayView out of App and into their own subcomponents

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,23 +3,33 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import * as actions from '../../actions';
-import APODcard from './APODcard';
-
+import Body from './Body';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment(),
+    };
+  }
+
   componentDidMount() {
     this.props.getAPOD();
+    this.props.submitDate(this.state.startDate.format('YYYY-MM-DD'));
   }
 
   render() {
     const {
       APOD,
+      submitDate,
+      asteroidList,
     } = this.props;
 
     return (
       <div className="App">
-        <APODcard data={APOD} />
+        <Body />
       </div>
     );
   }
@@ -28,10 +38,12 @@ class App extends Component {
 function mapStateToProps(state) {
   const {
     APOD,
+    asteroidList,
   } = state;
 
   return {
     APOD,
+    asteroidList,
   };
 }
 
