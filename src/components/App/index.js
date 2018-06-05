190277c559128e97e8ab7b0950d3780fc593d15f
Christopher Lee <xchrislee@gmail.com>
2018-06-05 14:44:57 -0500

    Add prop validation to root of App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import * as actions from '../../actions';
+import Navigation from './Navigation';
 import Body from './Body';
+
 
 class App extends Component {
   constructor(props) {
@@ -17,23 +18,39 @@ class App extends Component {
 
   componentDidMount() {
     this.props.submitDate(this.state.startDate.format('YYYY-MM-DD'));
-    this.props.getAPOD();
+    // this.props.getAPOD();
   }
 
   render() {
     const {
-      APOD,
-      submitDate,
+      // APOD,
+      // submitDate,
       neo,
     } = this.props;
 
     return (
       <div className="App">
+        <Navigation />
         <Body neo={neo} />
       </div>
     );
   }
 }
+
+App.propTypes = {
+  // APOD: PropTypes.shape({
+  //   date: PropTypes.string,
+  //   explanation: PropTypes.string,
+  //   hdurl: PropTypes.string,
+  //   media_type: PropTypes.string,
+  //   service_version: PropTypes.string,
+  //   title: PropTypes.string,
+  //   url: PropTypes.string,
+
+  // }),
+  submitDate: PropTypes.func.isRequired,
+  neo: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 function mapStateToProps(state) {
   const {
