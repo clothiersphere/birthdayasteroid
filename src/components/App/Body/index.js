3df7a161af121bb147faa61661684a31c36b4402
Christopher Lee <xchrislee@gmail.com>
2018-06-05 17:43:50 -0500

    Modify component to handle birthday routing

--- src/components/App/Body/index.js
+++ src/components/App/Body/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomeView from './HomeView';
+import BirthdayView from './BirthdayView';
+
 
 export default class Body extends Component {
   constructor(props) {
@@ -16,6 +18,7 @@ export default class Body extends Component {
     return (
       <div className="body">
         <Switch>
+          <Route exact path="/birthdayAsteroid" render={routeProps => <BirthdayView {...routeProps} {...props} />} />
           <Route exact path="/" render={routeProps => <HomeView {...routeProps} {...props} />} />
         </Switch>
       </div>
