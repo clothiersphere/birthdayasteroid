db25d88dc749167fa2448ec6a80ac6cb153ed1cd
Christopher Lee <xchrislee@gmail.com>
2018-06-06 17:37:05 -0500

    Modify component to display input icons Modify CSS to center submit button

--- src/components/App/Body/BirthdayView.js
+++ src/components/App/Body/BirthdayView.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import { Button } from 'semantic-ui-react';
+import { Button, Dropdown, Form, Segment, Label, Icon } from 'semantic-ui-react';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 
-import APODcard from './APODcard';
+// import APODcard from './APODcard';
 
-class BirthdayView extends Component {
+export default class BirthdayView extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -23,28 +23,53 @@ class BirthdayView extends Component {
 
   render() {
     const {
-      APOD,
+      // APOD,
       submitDate,
-      asteroidList,
+      neo,
     } = this.props;
+
+    // const months = [];
+    // const days = [];
+    // for (let i = 1; i <= 12; i += 1) {
+    //   months.push({
+    //     text: i,
+    //     value: i,
+    //   });
+    // }
+
+    // for (let i = 1; i <= 31; i += 1) {
+    //   days.push({
+    //     text: i,
+    //     value: i,
+    //   });
+    // }
+
+    // <Dropdown placeholder="Select Month" fluid selection options={months} />
+    // <Dropdown placeholder="Select Days" fluid selection options={days} />
+
 
     return (
       <div className="birthday-view">
-        <div className="datePicker">
-          <DatePicker
-            placeholderText="Select a birthday date"
-            selected={this.state.startDate}
-            onChange={this.handleChange}
-            minDate={moment().dayOfYear(1)}
-            maxDate={moment().dayOfYear(365)}
-            todayButton="Today"
-          />
-          <Button
-            onClick={() => submitDate(this.state.startDate.format('YYYY-MM-DD'))}
-          >
-          HAPPYBIRTHDAY
-          </Button>
-        </div>
+        <Segment inverted>
+          <Form inverted>
+            <Form.Input fluid >
+              <Icon name="birthday" position="left" inverted />
+              <DatePicker
+
+                placeholderText="Select a birth date"
+              />
+            </Form.Input>
+            <Form.Input icon="mobile" iconPosition="left" fluid label="recipient's mobile #" placeholder="mobile number" />
+            <Form.Input icon="mail" iconPosition="left" fluid label="sender's e-mail address" placeholder="sender's email address" />
+            <div className="birthday-button">
+              <Button
+                onClick={() => submitDate(this.state.startDate.format('YYYY-MM-DD'))}
+              >
+              Submit
+              </Button>
+            </div>
+          </Form>
+        </Segment>
       </div>
     );
   }
