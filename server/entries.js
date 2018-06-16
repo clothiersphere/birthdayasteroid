1afc512cab38dd06e325b8542b211b774cc2d517
Christopher Lee <xchrislee@gmail.com>
2018-06-15 19:48:36 -0700

    Implement ability to create birthday notification on db w/ mobile number validation

--- server/entries.js
+++ server/entries.js
@@ -1,69 +1,41 @@
 const axios = require('axios');
 const moment = require('moment');
 const pgp = require('pg-promise')();
-const _ = require('lodash');
 const PQ = require('pg-promise').ParameterizedQuery;
 
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/entries';
 
 const db = pgp(connectionString);
 
+
 function createNotification(req, res, next) {
   const { birthday, senderEmail, recipientMobile } = req.body;
-  console.log(recipientMobile, 'recipientMobile');
-
-  const findEntry = new PQ('SELECT * FROM entry WHERE mobile = $1', [recipientMobile]);
-  const addEntry = new PQ('INSERT INTO entry(birthday, mobile, sender_email) VALUES($1,$2,$3)');
+  const addEntry = new PQ('INSERT INTO entry(birthday, mobile, sender_email) VALUES($1,$2,$3) RETURNING mobile');
   addEntry.values = [birthday, recipientMobile, senderEmail];
 
+  db.task(t => t.oneOrNone('SELECT id FROM entry WHERE mobile = $1', recipientMobile)
+    .then(entry => entry || t.one(addEntry)))
+    .then((data) => {
+      if (Object.prototype.hasOwnProperty.call(data, 'id')) {
+        res.status(409).send({
+          success: false,
+          flag: 'entry_exists',
+        });
+        return next();
+      }
 
-  db.one(findEntry)
-    .then((entry) => {
-      res.send(`${entry.mobile} already exists`);
-      next();
+      if (Object.prototype.hasOwnProperty.call(data, 'mobile')) {
+        res.status(201).send({
+          success: true,
+        });
+        return next();
+      }
     })
     .catch((error) => {
-      db.none(addEntry)
-        .then((entry) => {
-
-        })
-        .catch((error) => {
-          console.log('ERROR:', error); // print error;
-        });
+      console.log('error:', error);
+      return next();
     });
 }
-
-// return db.any('select * from entry where mobile=$1', [recipientMobile])
-//   .then((data) => {
-//     res.send(data);
-//     next();
-//   })
-//   .catch((error) => {
-//   // db.one('INSERT INTO users(name, active) VALUES($1, $2) RETURNING id', ['John', true])
-//   //     .then(data => {
-//   //         console.log(data.id); // print new user id;
-//   //     })
-//   //     .catch(error => {
-//   //         console.log('ERROR:', error); // print error;
-//   //     });
-//     console.log('ERROR:', error);
-//   });
-// }
-
-// function createNotification(req, res, next) {
-//   return db.any('select * from entry')
-//     .then((data) => {
-//       console.log('DATA', data);
-//       res.send(data);
-//       next();
-//     })
-//     .catch((error) => {
-//       console.log('ERROR:', error);
-//     })
-//     .then(() => {
-//       pgp.end();
-//     });
-// }
 
 module.exports = {
   createNotification,
